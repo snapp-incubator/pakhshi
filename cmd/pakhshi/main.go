79aa64ffdd64d4878a5e94e1550a8bcb0b6bc50d
Parham Alvani <parham.alvani@gmail.com>
2021-06-06 13:07:26 +0430

    feat: Add Pakhshi cmd

---
+++ cmd/pakhshi/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/1995parham/pakhshi/internal/cmd"
+	"github.com/pterm/pterm"
+)
+
+func main() {
+	if err := pterm.DefaultBigText.WithLetters(
+		pterm.NewLettersFromStringWithStyle("Pa", pterm.NewStyle(pterm.FgCyan)),
+		pterm.NewLettersFromStringWithStyle("kh", pterm.NewStyle(pterm.FgLightMagenta)),
+		pterm.NewLettersFromStringWithStyle("shi", pterm.NewStyle(pterm.FgLightRed)),
+	).Render(); err != nil {
+		_ = err
+	}
+
+	pterm.Description.Println("One Client to Rule Them All")
+
+	cmd.Execute()
+}
